f2af3ccf005c718b2df44539a30fdc7d8684cff6
Gaurav-Jagnani <gaurav.jagnani5@yahoo.com>
2026-06-21 19:32:32 +0530

    App

--- backend/db.py
+++ backend/db.py
@@ -3,11 +3,9 @@ from sqlalchemy import create_engine
 
 def get_connection():
     # use for docker
-    # engine = create_engine(
-    #     "postgresql://postgres:postgres@postgres:5432/library_db"
-    # )
+    engine = create_engine("postgresql://postgres:postgres@postgres:5432/library_db")
     # use for local
-    engine = create_engine("postgresql://postgres:postgres@localhost:5432/library_db")
+    # engine = create_engine("postgresql://postgres:postgres@localhost:5432/library_db")
     conn = engine.connect()
 
     try:
